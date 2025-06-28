1010833733d609938b337b73260f6aaf90082650
Chijioke Elijah <elijahchijioke.ce@gmail.com>
2025-06-28 21:15:29 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <h1 className='text-xs text-red-500 font-serif'>Hello World</h1>
+    <h1 className='text-sm text-red-500 font-sans'>Hello World</h1>     
+     
+    </>
+  )
+}
+
+export default App
