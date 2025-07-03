ddc6f34e1a9d9a74957b61a1370c4bce3ac6b5be
Chijioke Elijah <elijahchijioke.ce@gmail.com>
2025-07-03 13:45:12 +0100

    fix loader image spell error

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,11 @@
 // components/Loader.jsx
 
-import Loader_img from "../images/Loader_img.svg";
+import loader_img from "../images/loader_img.svg";
 
 export default function Loader() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-white">
-      <img src={Loader_img} alt="loader" />
+      <img src={loader_img} alt="loader" />
     </div>
   );
 }
