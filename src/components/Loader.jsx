47b3a41263bc03037f5e8d790c52c9c9155484d6
Chijioke Elijah <elijahchijioke.ce@gmail.com>
2025-07-03 13:37:14 +0100

    fix loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,8 +5,7 @@ import Loader_img from "../images/Loader_img.svg";
 export default function Loader() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-white">
-      <img src={Loader_img} alt="loader"/>  
-      
+      <img src={Loader_img} alt="loader" />
     </div>
   );
 }
